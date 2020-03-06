c898d4deba6b979d973ef54713e77c05f728cf1e
dhduan <1060795493@qq.com>
2020-03-06 23:58:19 +0800

    分页功能，添加功能解耦

--- app.py
+++ app.py
@@ -27,20 +27,49 @@ login_manager.login_view = "login"
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
-def show_todo_list():
+def show_todo_list(page=None):
+    if not page:
+        page = 1
     form = TodoListForm()
-    if request.method == 'GET':
+    if request.method == 'GET' and current_user.id == 1:
         todolists = TodoList.query.all()
-        return render_template('index.html', todolists=todolists, form=form)
+        return render_template('index.html', todolists=todolists,form=form)
+    elif current_user.id != 1:
+        tdl = TodoList.query.filter_by(user_id=current_user.id).order_by(TodoList.create_time.desc()).paginate(page=page, per_page=5)
+        return render_template('index.html', todolists=tdl.items,  pagination=tdl,form=form)
     else:
         if form.validate_on_submit():
-            todolist = TodoList(current_user.id, form.title.data, form.status.data)
+            todolist = TodoList(current_user.id, form.title.data, form.content.data, form.status.data, current_user.username)
             db.session.add(todolist)
             db.session.commit()
-            flash('You have add a new todo list')
+            flash('新内容添加成功！')
         else:
             flash(form.errors)
         return redirect(url_for('show_todo_list'))
+
+@app.route('/dolist<int:page>',methods=['GET'])
+@login_required
+def show_page(page=None):
+    # 每个人只能看自己发表的
+    if not page:
+        page = 1
+    form = TodoListForm()
+    # paginate方法返回一个sqlalchemy.Pagination类型对象
+    blogs = TodoList.query.filter_by(user_id=current_user.id).order_by(TodoList.create_time.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', todolists=blogs.items, form=form, pagination=blogs)
+
+@app.route('/add',methods=['POST'])
+@login_required
+def add_todo_list():
+    form = TodoListForm()
+    if request.method =='POST':
+        if form.validate_on_submit():
+            todolist = TodoList(current_user.id, form.title.data, form.content.data, form.status.data,current_user.username)
+            db.session.add(todolist)
+            db.session.commit()
+            flash(current_user.username+'添加了一条新的工作内容')
+        else:flash(form.errors)
+    return redirect(url_for('show_todo_list'))
 
 
 @app.route('/delete/<int:id>')
@@ -61,6 +90,7 @@ def change_todo_list(id):
         form = TodoListForm()
         form.title.data = todolist.title
         form.status.data = str(todolist.status)
+        todolist.content = form.content.data
         return render_template('modify.html', form=form)
     else:
         form = TodoListForm()
@@ -68,8 +98,9 @@ def change_todo_list(id):
             todolist = TodoList.query.filter_by(id=id).first_or_404()
             todolist.title = form.title.data
             todolist.status = form.status.data
+            todolist.content = form.content.data
             db.session.commit()
-            flash('You have modify a todolist')
+            flash(current_user.username+'修改了第'+str(id)+'条记录')
         else:
             flash(form.errors)
         return redirect(url_for('show_todo_list'))
